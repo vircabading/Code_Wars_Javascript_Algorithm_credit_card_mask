a259874988acce783fd16174817ef41787ee4899
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-27 10:27:36 -0600

    add test cases

--- algo.js
+++ algo.js
@@ -10,18 +10,16 @@
  */
 function maskify(cc) {
     let output = "";
-    if (cc.length > 4) {
-        console.log("**** In If ****")
-        for (let idx = 0; idx < cc.length; idx++) {
-            if (idx < (cc.length-4)) {
-                output += "#";
-            } else {
-                output += cc[idx]
-            }
-            console.log(output[idx]);
-        }        
-    }
+    for (let idx = 0; idx < cc.length; idx++) {
+        if (idx < (cc.length-4)) {
+            output += "#";
+        } else {
+            output += cc[idx]
+        }
+    }        
     return output;
 }
 
 console.log("maskify 4556364607935616:", maskify("4556364607935616"));
+console.log("maskify 35616:", maskify("35616"));
+console.log("maskify 616:", maskify("616"));
