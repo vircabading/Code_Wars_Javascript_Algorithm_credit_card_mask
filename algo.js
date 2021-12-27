32b47db5a9dd2ca7e0e937228f75b9487bfcceb9
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-27 10:24:28 -0600

    create an output string to return as masked string

--- algo.js
+++ algo.js
@@ -9,14 +9,19 @@
  * @returns a masked string
  */
 function maskify(cc) {
+    let output = "";
     if (cc.length > 4) {
         console.log("**** In If ****")
-        for (let idx = 0; idx < cc.length-4; idx++) {
-            cc[idx] = "#";
-            console.log(cc[idx]);
+        for (let idx = 0; idx < cc.length; idx++) {
+            if (idx < (cc.length-4)) {
+                output += "#";
+            } else {
+                output += cc[idx]
+            }
+            console.log(output[idx]);
         }        
     }
-    return cc;
+    return output;
 }
 
 console.log("maskify 4556364607935616:", maskify("4556364607935616"));
